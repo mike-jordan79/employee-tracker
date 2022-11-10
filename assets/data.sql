c426edad3e9f52df693b6960c5fac8f4b9405491
mike-jordan79 <mike_jordan79@yahoo.com>
2022-11-10 13:05:40 -0500

    added more into file

--- assets/data.sql
+++ assets/data.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS `employee_tracker_DB`;
 CREATE DATABASE `employee_tracker_DB`;
 USE `employee_tracker_DB`;
 
+CREATE TABLE IF NOT EXISTS `department` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(30),
+    PRIMARY KEY(`id`)
+);
+
